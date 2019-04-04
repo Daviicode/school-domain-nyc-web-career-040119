072a9901f5606621fe6b290ce709a7edec3c5d96
David zhang <d.ydzdavid@yahoo.com>
2019-04-04 12:51:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+  
+  attr_accessor :student_name, :roster
+  def initialize (student_name) 
+    @roster = []
+    @student_name = student_name
+    
+  end
+  
+  def roster
+    @roster
+  end 
+  
+  def add_student(student_name, grade)
+      @roster[grade] != nil
+      @roster[grade] << student_name
+  end
+  
+end
